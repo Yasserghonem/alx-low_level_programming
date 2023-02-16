c492b4d2cee9cefd1f373f87da8b44ae5f87d622
Yasserghonem <yasserghonem56@gmail.com>
2023-02-16 12:26:20 -0800

    size of

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main block 
+ * Return: 0
+ */
+int main(void)
+{
+	printf("size of char : %c", sizeof(char));
+	printf("size of int : %d", sizeof(int));
+	printf("size of long int : %ld", sizeof(long int));
+	printf("size of long long int : %lld", sizeof(long long int));
+	return (0);
+}
