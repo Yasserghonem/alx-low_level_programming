b9f3197e0a7ea5494b8830b123cd74f6410d57fd
Yasserghonem <yasserghonem56@gmail.com>
2023-02-21 09:37:52 -0800

    fff

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * Returns 1 and prints + if n is greater than zero
+ * Returns 0 and prints 0 if n is zero
+ * Returns -1 and prints - if n is less than zero
+ *@n: the int to print
+ * Return: Always 0.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	else
+	{
+		return (-1);
+		_putchar('-');
+	}
+}
