cedf76a167e366789baea517c0245f7e52f3aeaa
Yasserghonem <yasserghonem56@gmail.com>
2023-02-27 10:06:12 -0800

    78910

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *  *puts_half - prints every other character of a string
- *   *@str: A pointer to an int that will be changed
- *    *
- *     *Return: void which means our answer is correct
- *      */
+ * puts_half - prints every other character of a string
+ * @str: A pointer to an int that will be changed
+ *
+ * Return: void which means our answer is correct
+ */
 
 void puts_half(char *str)
 
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *  *print_array - prints n elements of an array of integers
- *   *@a: A pointer to an int that will be updated/ changed
- *    *@n: return value n
- *     *Return: void which ,means our answer is correct
- *      */
+ * print_array - prints n elements of an array of integers
+ * @a: A pointer to an int that will be updated/ changed
+ * @n: return value n
+ * Return: void which ,means our answer is correct
+ */
 
 void print_array(int *a, int n)
 
