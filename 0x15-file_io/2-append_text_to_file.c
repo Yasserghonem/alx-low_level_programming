2a2824edd1bd0df1148531e1d1378af74283a1c2
Yasserghonems <yasserghonem56@gmail.com>
2023-04-10 04:02:36 -0700

    sss

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,41 +1,29 @@
 #include "main.h"
 
 /**
- * _strlen - finds the length of a string
- * @str: pointer to the string
- *
- * Return: length of the string
- */
-size_t _strlen(char *str)
-{
-	size_t i;
-
-	for (i = 0; str[i]; i++)
-		;
-	return (i);
-}
-
-/**
- * append_text_to_file - appends a text at the end of a file.
- * @filename: name of the file
- * @text_content: NULL terminated string to add at the end of the file
- *
- * Return: 1 on success and -1 on failure
+ * append_text_to_file - appends text at the end of a file
+ * @filename: the name of the file
+ * @text_content: the NULL terminated string to add at end of file
+ * Return: 1 on success, -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t len;
+    int lol, fwrite, i;
 
-	if (filename == NULL)
-		return (-1);
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
-		return (-1);
-	if (text_content != NULL)
-		len = write(fd, text_content, _strlen(text_content));
-	close(fd);
-	if (len == -1)
-		return (-1);
-	return (1);
+    if (filename == NULL)
+        return (-1);
+    lol = open(filename, O_APPEND | O_RDWR, 0664);
+    if (lol == -1)
+        return (-1);
+
+    if (text_content != NULL)
+    {
+        for (i = 0; text_content[i]; i++)
+            ;
+        fwrite = write(lol, text_content, i);
+        if (fwrite == -1)
+            return (-1);
+    }
+    close(lol);
+    return (1);
 }
